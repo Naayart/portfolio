1db16092f09e3e887b8c58eb769fbd5b503408ac
Naayart <jerusha328@gmail.com>
2025-05-19 04:40:54 -0700

    continuation of js

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,13 @@
+// declare variable to store nav bar element
+const navElement = document.getElementById("navbar");
 // detect scroll event on document
 document.addEventListener("scroll", function () {
   // console.log(window.scrollY);
   if (window.scrollY > 70) {
     // change navbar bg to blue
-    document.getElementById("navbar").style.backgroundColor = "#0779e4";
+    navElement.style.backgroundColor = "#0779e4";
   } else {
     // change navbar bg to transparent
-    document.getElementById("navbar").style.backgroundColor = "transparent";
+    navElement.style.backgroundColor = "transparent";
   }
 });
